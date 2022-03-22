ea19fb3cd000e9960eae0cdbc8023866f50f52a6
Joker-V2 <nipinna0@gmail.com>
2022-03-22 11:36:56 +0000

    violet: LineageOS bringup

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -8,17 +8,14 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common Lineage OS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Charging Animation
-TARGET_INCLUDE_PIXEL_CHARGER := true
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_violet
+PRODUCT_NAME := lineage_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
@@ -31,6 +28,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="violet"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# SuperiorOS
-SUPERIOR_OFFICIAL := true
